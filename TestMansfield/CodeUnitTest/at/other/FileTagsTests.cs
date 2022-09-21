8d0ecb8a57e5fe32d8c44a27d1477511117a7a5a
Xiaofeng Zhang <xiaofeng.zhang@ceridian.com>
2022-09-22 09:56:26 +1000

    vscode, test

--- TestMansfield/CodeUnitTest/at/other/FileTagsTests.cs
+++ TestMansfield/CodeUnitTest/at/other/FileTagsTests.cs
@@ -1,55 +1,55 @@
-﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using AllChallenge.at;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AllChallenge.at;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace AllChallenge.at.Tests
-//{
-//    [TestClass()]
-//    public class FileTagsTests
-//    {
-//        [TestMethod()]
-//        public void GroupFileTagTest1()
-//        {
-//            string[] fileNames = { "file1", "file2", "file3" };
-//            List<string[]> tags = new List<string[]>
-//                                {
-//                                    new[]{ "cat", "dog", "ant"},
-//                                    new[]{ "dog", "frog"},
-//                                    new[]{ "dog", "cat"}
-//                                };
-//            int[] sizes = { 1, 2, 3 };
-//            string[] expected = { "dog", "cat", "frog", "ant" };
-//            var fileTags = new FileTags();
-//            CollectionAssert.AreEquivalent(expected,
-//                fileTags.GroupFileTag(fileNames, tags, sizes).ToArray());
-//        }
+namespace AllChallenge.at.Tests
+{
+    [TestClass()]
+    public class FileTagsTests
+    {
+        [TestMethod()]
+        public void GroupFileTagTest1()
+        {
+            string[] fileNames = { "file1", "file2", "file3" };
+            List<string[]> tags = new List<string[]>
+                                {
+                                    new[]{ "cat", "dog", "ant"},
+                                    new[]{ "dog", "frog"},
+                                    new[]{ "dog", "cat"}
+                                };
+            int[] sizes = { 1, 2, 3 };
+            string[] expected = { "dog", "cat", "frog", "ant" };
+            var fileTags = new FileTags();
+            CollectionAssert.AreEquivalent(expected,
+                fileTags.GroupFileTag(fileNames, tags, sizes).ToArray());
+        }
 
-//        [TestMethod()]
-//        public void GroupFileTagTest2()
-//        {
-//            string[] fileNames = { "file1", "file2", "file3" };
-//            List<string[]> tags = new List<string[]>
-//                                {
-//                                    new[]{ "cat", "dog", "ant"},
-//                                    new[]{ "dog", "ant"},
-//                                    new[]{ "dog", "cat"}
-//                                };
-//            int[] sizes = { 1, 2, 3 };
+        [TestMethod()]
+        public void GroupFileTagTest2()
+        {
+            string[] fileNames = { "file1", "file2", "file3" };
+            List<string[]> tags = new List<string[]>
+                                {
+                                    new[]{ "cat", "dog", "ant"},
+                                    new[]{ "dog", "ant"},
+                                    new[]{ "dog", "cat"}
+                                };
+            int[] sizes = { 1, 2, 3 };
 
-//            /*
-//                cat: (file1) 1 + (file2) 2 + (file3) + 3 = 6
-//                dog: (file1) 1 + (file2) 2 + (file3) + 3 = 6
-//                ant: (file1) 1 + (file2) 2 = 3
-//             */
-//            string[] expected = { "cat", "dog", "ant" };
+            /*
+                cat: (file1) 1 + (file2) 2 + (file3) + 3 = 6
+                dog: (file1) 1 + (file2) 2 + (file3) + 3 = 6
+                ant: (file1) 1 + (file2) 2 = 3
+             */
+            string[] expected = { "cat", "dog", "ant" };
 
-//            var fileTags = new FileTags();
-//            CollectionAssert.AreEquivalent(expected,
-//                fileTags.GroupFileTag(fileNames, tags, sizes).ToArray());
-//        }
-//    }
-//}
+            var fileTags = new FileTags();
+            CollectionAssert.AreEquivalent(expected,
+                fileTags.GroupFileTag(fileNames, tags, sizes).ToArray());
+        }
+    }
+}
